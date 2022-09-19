b24e36569f0a1b70b8b2c1ea977bc799deec5fc0
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-09-19 16:11:58 +0530

    send chat message

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+
+// Express initializes app to be a function handler that you can supply to an HTTP server
 const server = http.createServer(app);
+
+// Initialize a new instance of socket.io by passing the server (the HTTP server) object. 
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// redirect to index.html directory
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
 server.listen(3000, () => {
   console.log('listening on 3000...');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
